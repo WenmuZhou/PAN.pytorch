517e9eec3eeb629a9f346f2a80599b0e01e653ff
zhoujun <572459439@qq.com>
2019-11-29 14:25:11 +0800

    fix bug on add graph

--- post_processing/__init__.py
+++ post_processing/__init__.py
@@ -63,3 +63,30 @@ def decode(preds, scale=1, threshold=0.7311, min_area=5):
         bbox = cv2.boxPoints(rect)
         bbox_list.append([bbox[1], bbox[2], bbox[3], bbox[0]])
     return pred, np.array(bbox_list)
+
+
+def decode_dice(preds, scale=1, threshold=0.7311, min_area=5):
+    import pyclipper
+    preds[:2, :, :] = torch.sigmoid(preds[:2, :, :])
+    preds = preds.detach().cpu().numpy()
+    text = preds[0] > threshold  # text
+    kernel = (preds[1] > threshold) * text  # kernel
+
+    label_num, label = cv2.connectedComponents(kernel.astype(np.uint8), connectivity=4)
+    bbox_list = []
+    for label_idx in range(1, label_num):
+        points = np.array(np.where(label_num == label_idx)).transpose((1, 0))[:, ::-1]
+
+        rect = cv2.minAreaRect(points)
+        poly = cv2.boxPoints(rect).astype(int)
+
+        d_i = cv2.contourArea(poly) * 1.5 / cv2.arcLength(poly, True)
+        pco = pyclipper.PyclipperOffset()
+        pco.AddPath(poly, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
+        shrinked_poly = np.array(pco.Execute(-d_i))
+
+        if cv2.contourArea(shrinked_poly) < 800 / (scale * scale):
+            continue
+
+        bbox_list.append([shrinked_poly[1], shrinked_poly[2], shrinked_poly[3], shrinked_poly[0]])
+    return label, np.array(bbox_list)
--- predict.py
+++ predict.py
@@ -9,7 +9,39 @@ import cv2
 import time
 
 from models import get_model
+
 from post_processing import decode
+
+def decode_clip(preds, scale=1, threshold=0.7311, min_area=5):
+    import pyclipper
+    import numpy as np
+    preds[:2, :, :] = torch.sigmoid(preds[:2, :, :])
+    preds = preds.detach().cpu().numpy()
+    text = preds[0] > threshold  # text
+    kernel = (preds[1] > threshold) * text  # kernel
+
+    label_num, label = cv2.connectedComponents(kernel.astype(np.uint8), connectivity=4)
+    bbox_list = []
+    for label_idx in range(1, label_num):
+        points = np.array(np.where(label == label_idx)).transpose((1, 0))[:, ::-1]
+        if points.shape[0] < min_area:
+            continue
+        rect = cv2.minAreaRect(points)
+        poly = cv2.boxPoints(rect).astype(int)
+
+        d_i = cv2.contourArea(poly) * 1.5 / cv2.arcLength(poly, True)
+        pco = pyclipper.PyclipperOffset()
+        pco.AddPath(poly, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
+        shrinked_poly = np.array(pco.Execute(d_i))
+        if shrinked_poly.size == 0:
+            continue
+        rect = cv2.minAreaRect(shrinked_poly)
+        shrinked_poly = cv2.boxPoints(rect).astype(int)
+        if cv2.contourArea(shrinked_poly) < 800 / (scale * scale):
+            continue
+
+        bbox_list.append([shrinked_poly[1], shrinked_poly[2], shrinked_poly[3], shrinked_poly[0]])
+    return label, np.array(bbox_list)
 
 
 class Pytorch_model:
@@ -26,7 +58,7 @@ class Pytorch_model:
         else:
             self.device = torch.device("cpu")
         print('device:', self.device)
-        checkpoint = torch.load(model_path,map_location=self.device)
+        checkpoint = torch.load(model_path, map_location=self.device)
 
         config = checkpoint['config']
         config['arch']['args']['pretrained'] = False
@@ -77,13 +109,13 @@ if __name__ == '__main__':
 
     os.environ['CUDA_VISIBLE_DEVICES'] = str('0')
 
-    model_path = 'output/PAN_pred_mask_resnet50/checkpoint/model_best.pth'
+    model_path = 'output/PAN_shufflenetv2_FPEM_FFM.pth'
 
     img_id = 10
     img_path = 'E:/zj/dataset/icdar2015/test/img/img_{}.jpg'.format(img_id)
 
     # 初始化网络
-    model = Pytorch_model(model_path, gpu_id=None)
+    model = Pytorch_model(model_path, gpu_id=0)
     preds, boxes_list, t = model.predict(img_path)
     show_img(preds)
     img = draw_bbox(cv2.imread(img_path)[:, :, ::-1], boxes_list)
